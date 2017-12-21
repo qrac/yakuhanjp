a943f8dfd7fea0eb4c37d6286e78fab0d07665eb
Qrac <qrac.jp@gmail.com>
2017-12-21 13:20:27 +0900

    Fix demo

--- docs/js/app.js
+++ docs/js/app.js
@@ -159,3 +159,17 @@ $(function () {
     }
   });
 });
+//----------------------------------------------------
+// Smooth scroll
+//----------------------------------------------------
+
+$(function () {
+  $('a[href^="#"]').on('click', function () {
+    var speed = 400;
+    var href = $(this).attr('href');
+    var target = $(href == '#' || href == '' ? 'html' : href);
+    var position = target.offset().top;
+    $('body,html').animate({ scrollTop: position }, speed, 'swing');
+    return false;
+  });
+});
