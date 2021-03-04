9214dbf28a635bfe1579d5343b20bf5dcf929c9d
Qrac <qrac.jp@gmail.com>
2021-03-04 14:28:07 +0900

    Build scss template #35

--- packages/yakuhanjp/src/index.js
+++ packages/yakuhanjp/src/index.js
@@ -1,8 +1,50 @@
-const fs = require("fs");
-const pkg = JSON.parse(fs.readFileSync("./package.json", "utf8"));
-const pjt = JSON.parse(fs.readFileSync("./project.json", "utf8"));
-const tmp = fs.readFileSync("./src/template.scss", "utf8");
+const fs = require("fs")
 
-//console.log(pkg);
-//console.log(pjt);
-console.log(tmp);
+const pkg = JSON.parse(fs.readFileSync("./package.json", "utf8"))
+const pjt = JSON.parse(fs.readFileSync("./project.json", "utf8"))
+const tmp = fs.readFileSync("./src/template.scss", "utf8")
+const distScssDir = "./dist/scss"
+
+const glyphs2unicode = (glyphs) => {
+  const splitGlyphs = glyphs.split("")
+  const arrayUnicode = []
+  splitGlyphs.forEach((str) => {
+    arrayUnicode.push(str2unicode(str))
+  })
+  arrayUnicode.sort()
+  const joinUnicode = arrayUnicode.join()
+  return joinUnicode
+}
+
+const str2unicode = (str) => {
+  return "U+" + ("000" + str.charCodeAt(0).toString(16)).substr(-4)
+}
+
+const weights2scssWeights = (weights) => {
+  const arrayWeights = []
+  Object.keys(weights).forEach((key) => {
+    arrayWeights.push(`${key}:"${weights[key]}"`)
+  })
+  const joinWeights = arrayWeights.join()
+  return joinWeights
+}
+
+if (!fs.existsSync(distScssDir)) {
+  fs.mkdirSync(distScssDir)
+}
+
+pjt.fonts.forEach((font) => {
+  const unicode = glyphs2unicode(font.glyphs)
+  const scssWeights = weights2scssWeights(font.weights)
+  data = tmp.replace(/___pjtName___/g, pjt.name)
+  data = data.replace(/___pkgVersion___/g, pkg.version)
+  data = data.replace(/___pkgLicense___/g, pkg.license)
+  data = data.replace(/___pkgAuthorName___/g, pkg.author.name)
+  data = data.replace(/___fontName___/g, font.name)
+  data = data.replace(/___fontBase___/g, font.base)
+  data = data.replace(/___fontDistVariable___/g, font.dist.variable)
+  data = data.replace(/___fontFile___/g, font.file)
+  data = data.replace(/___unicode___/g, unicode)
+  data = data.replace(/___weights___/g, scssWeights)
+  fs.writeFileSync(`${distScssDir}/${font.dist.name}.scss`, data)
+})
