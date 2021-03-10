d8af687b3193358baf9b24b55993a3cc3f549e2f
Qrac <qrac.jp@gmail.com>
2021-03-10 21:23:55 +0900

    Test docs font loading

--- docs/src/components/app-layout.js
+++ docs/src/components/app-layout.js
@@ -76,7 +76,19 @@ const AppLayout = ({ children }) => {
         <link
           rel="stylesheet"
           href={
-            "https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@100;300;400;500;700;800;900&family=Noto+Sans+JP:wght@100;300;400;500;700;900&family=Noto+Serif+JP:wght@200;300;400;500;600;700;900&display=swap"
+            "https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;300;400;500;700;900&display=swap"
+          }
+        />
+        <link
+          rel="stylesheet"
+          href={
+            "https://fonts.googleapis.com/css2?family=Noto+Serif+JP:wght@200;300;400;500;600;700;900&display=swap"
+          }
+        />
+        <link
+          rel="stylesheet"
+          href={
+            "https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@100;300;400;500;700;800;900&display=swap"
           }
         />
       </Helmet>
