20cbafea9c9051bf7c30937f3cbabd429704165e
Qrac <qrac.jp@gmail.com>
2023-08-27 18:23:03 +0900

    Test css build #69 (#70)

---
+++ packages/yakuhanjp/vite.config.ts
@@ -0,0 +1,40 @@
+import type { PreRenderedAsset } from "rollup"
+import path from "node:path"
+import { defineConfig } from "vite"
+import banner from "vite-plugin-banner"
+
+import pkg from "./package.json"
+import pjt from "./project.json"
+
+function resolveOutput(chunkInfo: PreRenderedAsset) {
+  const filePath = chunkInfo.name || ""
+  const fileExt = path.extname(filePath)
+
+  if (fileExt === ".woff2") {
+    const fontName = filePath.split("-")[0]
+    return path.join("fonts", fontName, "[name]" + ".[ext]")
+  }
+  return path.join("css", "[name]" + ".[ext]")
+}
+
+export default defineConfig({
+  base: "./",
+  build: {
+    assetsInlineLimit: 0,
+    rollupOptions: {
+      input: ["./src/yakuhanjp.css"],
+      output: {
+        assetFileNames: (chunkInfo) => resolveOutput(chunkInfo),
+      },
+    },
+  },
+  plugins: [
+    banner((filePath: string) => {
+      const fileName = path.basename(filePath).split(".")[0]
+      const font = pjt.fonts.find((item) => item.dist.name === fileName)
+      const pkgStr = `${pjt.name} v${pkg.version} ${pkg.license} by ${pkg.author.name}`
+      const typeStr = `Type: ${font?.name} - Based on ${font?.base}`
+      return `${pkgStr} | ${typeStr}`
+    }),
+  ],
+})
